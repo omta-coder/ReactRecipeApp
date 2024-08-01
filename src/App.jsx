f35bc72556ff7f81e8f01b6ea865f8b4271de45d
Omta <omtadayma2001@gmail.com>
2024-08-01 18:06:39 +0530

    Create Components

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,24 @@
 import React from 'react'
+import { Route, Routes } from 'react-router-dom'
+import Nav from './components/Nav'
+import Recipes from './components/Recipes'
+import About from './components/About'
+import Contact from './components/Contact'
+import Layout from './components/Layout'
+import Create from './components/Create'
 
 const App = () => {
   return (
-    <div className='text-3xl'>App</div>
+    <div  className="w-[80%] m-auto ">
+      <Nav/>
+      <Routes>
+        <Route path='/' element={<Layout/>}/>
+        <Route path='/create-recipe' element={<Create/>}/>
+        <Route path='/recipes' element={<Recipes/>}/>
+        <Route path='/about' element={<About/>}/>
+        <Route path='/contact' element={<Contact/>}/>
+      </Routes>
+    </div>
   )
 }
 
---
+++ src/components/Layout.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import Home from './Home'
+import Recipes from './Recipes'
+import Footer from './Footer'
+
+const Layout = () => {
+  return (
+    <>
+    <Home/>
+    <Recipes/>
+    <Footer/>
+    </>
+  )
+}
+
+export default Layout
