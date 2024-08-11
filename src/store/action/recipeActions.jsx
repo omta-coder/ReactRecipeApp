fa418d670d9e690e670e341b7c7774cb0879be94
Omta <omtadayma2001@gmail.com>
2024-08-11 15:04:28 +0530

    REDUX

--- src/store/action/recipeActions.jsx
+++ src/store/action/recipeActions.jsx
@@ -1,12 +1,10 @@
 import { addrecipe } from "../reducers/recipeSlice";
 
-
-export const asyncgetrecipies = () => async(dispatch,getState)=>{
+export const asyncgetrecipies = () => async (dispatch, getState) => {
     try {
-        const data = JSON.parse(localStorage.getItem("recipes")||[])
-        dispatch(addrecipe(data))
+        let data = JSON.parse(localStorage.getItem("recipes")) || [];
+        dispatch(addrecipe(data));
     } catch (error) {
-        console.log(error);
-        
+        toast.error(error);
     }
-}
+};
